be90a1397476c56173abfcb843507e162d06756f
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-09-05 15:32:20 +0200

    LPS-53189 Compare session age with password age
    Fail fast. Do not check default user (guest users). Compare primitives instead of Date#before to improve performance. Do not resubmit non GET requests. Use complete portal URL if rewritten by proxy / web server. Do not rethrow logged exception.

--- portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/password/modified/PasswordModifiedFilter.java
@@ -18,13 +18,15 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.session.AuthenticatedSessionManagerUtil;
+import com.liferay.portal.kernel.servlet.HttpMethods;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.servlet.filters.BasePortalFilter;
 
 import java.util.Date;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -40,58 +42,57 @@ public class PasswordModifiedFilter extends BasePortalFilter {
 			FilterChain filterChain)
 		throws Exception {
 
-		try {
-			if (_isPasswordChanged(request)) {
-				HttpSession session = request.getSession(false);
+		if (_isPasswordModified(request)) {
+			AuthenticatedSessionManagerUtil.logout(request, response);
 
-				session.invalidate();
+			String redirect = PortalUtil.getCurrentCompleteURL(request);
 
-				response.sendRedirect(request.getRequestURI());
+			if (!StringUtil.equals(request.getMethod(), HttpMethods.GET)) {
+				redirect = PortalUtil.getPortalURL(request);
 			}
-			else {
-				filterChain.doFilter(request, response);
-			}
+
+			response.sendRedirect(redirect);
 		}
-		catch (PortalException pe) {
-			throw new ServletException(pe);
+		else {
+			filterChain.doFilter(request, response);
 		}
 	}
 
-	private boolean _isPasswordChanged(HttpServletRequest request)
-		throws PortalException {
+	private boolean _isPasswordModified(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
 
-		HttpSession session = null;
-		User user = null;
+		if (session == null) {
+			return false;
+		}
 
 		try {
-			session = request.getSession(false);
-			user = PortalUtil.getUser(request);
+			User user = PortalUtil.getUser(request);
+
+			if ((user == null) || user.isDefaultUser()) {
+				return false;
+			}
+
+			Date userModifiedPasswordDate = user.getPasswordModifiedDate();
+
+			if (userModifiedPasswordDate == null) {
+				return false;
+			}
+
+			boolean sessionOlderThanPassword = false;
+
+			if (session.getCreationTime() <
+					userModifiedPasswordDate.getTime()) {
+
+				sessionOlderThanPassword = true;
+			}
+
+			return sessionOlderThanPassword;
 		}
 		catch (PortalException pe) {
 			_log.error(pe, pe);
 
-			throw pe;
+			return false;
 		}
-
-		if ((session != null) && (user != null)) {
-			Date sessionCreationDate = new Date(session.getCreationTime());
-			Date sessionModifiedPasswordDate = (Date)session.getAttribute(
-				"DATE_PASSWORD_CHANGED");
-			Date userModifiedPasswordDate = user.getPasswordModifiedDate();
-
-			if (sessionModifiedPasswordDate != null) {
-				if (sessionCreationDate.before(sessionModifiedPasswordDate)) {
-					return true;
-				}
-			}
-			else if ((userModifiedPasswordDate != null) &&
-					 sessionCreationDate.before(userModifiedPasswordDate)) {
-
-				return true;
-			}
-		}
-
-		return false;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
