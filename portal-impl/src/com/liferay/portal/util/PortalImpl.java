9b9f7fcbc20d0f9908c8929eba28384420f5a0c6
Marta Medio <marta.medio@liferay.com>
2019-04-16 09:33:32 +0200

    LPS-89566 Use of InetAddressProviderUtil instead InetAddressUtil (deprecated)

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -170,7 +170,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.util.InetAddressUtil;
+import com.liferay.portal.kernel.util.InetAddressProviderUtil;
 import com.liferay.portal.kernel.util.InheritableMap;
 import com.liferay.portal.kernel.util.JavaConstants;
 import com.liferay.portal.kernel.util.ListMergeable;
@@ -1000,8 +1000,8 @@ public class PortalImpl implements Portal {
 			}
 
 			try {
-				InetAddress inetAddress = InetAddressUtil.getInetAddressByName(
-					domain);
+				InetAddress inetAddress =
+					InetAddressProviderUtil.getInetAddressByName(domain);
 
 				String hostAddress = inetAddress.getHostAddress();
 
