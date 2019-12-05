88a0877d378d9f77e1dec4766197336719e47114
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2019-12-05 17:04:26 +0100

    LPS-101720 Add MFAVerifyMVCRenderCommand and JSP

---
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-checker-email-otp-web/src/main/java/com/liferay/multi/factor/authentication/checker/email/otp/web/internal/portlet/action/MFAVerifyMVCRenderCommand.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.multi.factor.authentication.checker.email.otp.web.internal.portlet.action;
+
+import com.liferay.multi.factor.authentication.checker.email.otp.web.internal.checker.EmailOTPMFAChecker;
+import com.liferay.multi.factor.authentication.checker.email.otp.web.internal.constants.MFAPortletKeys;
+import com.liferay.multi.factor.authentication.checker.email.otp.web.internal.constants.WebKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tomas Polesovsky
+ */
+@Component(
+	property = {
+		"javax.portlet.name=" + MFAPortletKeys.MFA_VERIFY_PORTLET,
+		"mvc.command.name=/mfa_verify/view"
+	},
+	service = MVCRenderCommand.class
+)
+public class VerifyMFARenderCommand implements MVCRenderCommand {
+
+	@Override
+	public String render(
+		RenderRequest renderRequest, RenderResponse renderResponse) {
+
+		long mfaUserId = _getMultiFactorAuthenticationUserId(renderRequest);
+
+		if (mfaUserId == 0) {
+			SessionErrors.add(renderRequest, "sessionExpired");
+
+			return "/error.jsp";
+		}
+
+		renderRequest.setAttribute(
+			WebKeys.EMAIL_MFA_CHECKER, _emailOTPMFAChecker);
+		renderRequest.setAttribute(WebKeys.MFA_USER_ID, mfaUserId);
+
+		return "/mfa_verify/verify.jsp";
+	}
+
+	private long _getMultiFactorAuthenticationUserId(
+		PortletRequest portletRequest) {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			com.liferay.portal.kernel.util.WebKeys.THEME_DISPLAY);
+
+		if (themeDisplay.isSignedIn()) {
+			return themeDisplay.getUserId();
+		}
+
+		HttpServletRequest httpServletRequest =
+			_portal.getOriginalServletRequest(
+				_portal.getHttpServletRequest(portletRequest));
+
+		HttpSession httpSession = httpServletRequest.getSession();
+
+		return GetterUtil.getLong(
+			httpSession.getAttribute(WebKeys.MFA_USER_ID));
+	}
+
+	@Reference
+	private EmailOTPMFAChecker _emailOTPMFAChecker;
+
+	@Reference
+	private Portal _portal;
+
+}
