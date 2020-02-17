428cff554888fc1df8ffd7d51b5ef0240573d6dd
Marta Medio <marta.medio@liferay.com>
2020-02-17 17:25:57 +0100

    LPS-105158 Show new tree view based on OmniAdmin Permissions

--- modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/portlet/action/AssignScopesMVCRenderCommand.java
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/portlet/action/AssignScopesMVCRenderCommand.java
@@ -19,17 +19,22 @@ import com.liferay.oauth2.provider.configuration.OAuth2ProviderConfiguration;
 import com.liferay.oauth2.provider.scope.liferay.ScopeLocator;
 import com.liferay.oauth2.provider.scope.liferay.spi.ApplicationDescriptorLocator;
 import com.liferay.oauth2.provider.scope.liferay.spi.ScopeDescriptorLocator;
+import com.liferay.oauth2.provider.scope.spi.scope.matcher.ScopeMatcherFactory;
 import com.liferay.oauth2.provider.service.OAuth2ApplicationScopeAliasesLocalService;
 import com.liferay.oauth2.provider.service.OAuth2ApplicationService;
 import com.liferay.oauth2.provider.service.OAuth2ScopeGrantLocalService;
 import com.liferay.oauth2.provider.web.internal.constants.OAuth2ProviderPortletKeys;
 import com.liferay.oauth2.provider.web.internal.constants.OAuth2ProviderWebKeys;
 import com.liferay.oauth2.provider.web.internal.display.context.AssignScopesDisplayContext;
+import com.liferay.oauth2.provider.web.internal.display.context.AssignScopesTreeDisplayContext;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -39,6 +44,7 @@ import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.ConfigurationPolicy;
@@ -63,19 +69,42 @@ public class AssignScopesMVCRenderCommand implements MVCRenderCommand {
 	public String render(
 		RenderRequest renderRequest, RenderResponse renderResponse) {
 
+		ThemeDisplay themeDisplay = getThemeDisplay(renderRequest);
+
 		try {
 			AssignScopesDisplayContext assignScopesDisplayContext =
 				new AssignScopesDisplayContext(
 					_oAuth2ApplicationService,
 					_oAuth2ApplicationScopeAliasesLocalService,
 					_oAuth2ScopeGrantLocalService, _oAuth2ProviderConfiguration,
-					renderRequest, getThemeDisplay(renderRequest),
-					_applicationDescriptorLocator, _scopeDescriptorLocator,
-					_scopeLocator, _dlURLHelper);
+					renderRequest, themeDisplay, _applicationDescriptorLocator,
+					_scopeDescriptorLocator, _scopeLocator, _dlURLHelper);
 
 			renderRequest.setAttribute(
 				OAuth2ProviderWebKeys.OAUTH2_ADMIN_PORTLET_DISPLAY_CONTEXT,
 				assignScopesDisplayContext);
+
+			PermissionChecker permissionChecker =
+				themeDisplay.getPermissionChecker();
+
+			if (!permissionChecker.isOmniadmin()) {
+				ScopeMatcherFactory scopeMatcherFactory =
+					getScopeMatcherFactory(themeDisplay.getCompanyId());
+
+				AssignScopesTreeDisplayContext assignScopesTreeDisplayContext =
+					new AssignScopesTreeDisplayContext(
+						_oAuth2ApplicationService,
+						_oAuth2ApplicationScopeAliasesLocalService,
+						_oAuth2ScopeGrantLocalService,
+						_oAuth2ProviderConfiguration, renderRequest,
+						themeDisplay, _scopeDescriptorLocator, _scopeLocator,
+						scopeMatcherFactory, _dlURLHelper);
+
+				renderRequest.setAttribute(
+					OAuth2ProviderWebKeys.
+						OAUTH2_ADMIN_PORTLET_TREE_DISPLAY_CONTEXT,
+					assignScopesTreeDisplayContext);
+			}
 		}
 		catch (PortalException portalException) {
 			if (_log.isDebugEnabled()) {
@@ -87,13 +116,38 @@ public class AssignScopesMVCRenderCommand implements MVCRenderCommand {
 	}
 
 	@Activate
-	protected void activate(Map<String, Object> properties) {
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
 		_oAuth2ProviderConfiguration = ConfigurableUtil.createConfigurable(
 			OAuth2ProviderConfiguration.class, properties);
+
+		_scopeMatcherFactoriesServiceTrackerMap =
+			ServiceTrackerMapFactory.openSingleValueMap(
+				bundleContext, ScopeMatcherFactory.class, "company.id");
+	}
+
+	protected ScopeMatcherFactory getScopeMatcherFactory(long companyId) {
+		ScopeMatcherFactory scopeMatcherFactory =
+			_scopeMatcherFactoriesServiceTrackerMap.getService(
+				String.valueOf(companyId));
+
+		if (scopeMatcherFactory == null) {
+			return _defaultScopeMatcherFactory;
+		}
+
+		return scopeMatcherFactory;
 	}
 
 	protected ThemeDisplay getThemeDisplay(PortletRequest portletRequest) {
 		return (ThemeDisplay)portletRequest.getAttribute(WebKeys.THEME_DISPLAY);
+	}
+
+	@Reference(name = "default", unbind = "-")
+	protected void setDefaultScopeMatcherFactory(
+		ScopeMatcherFactory defaultScopeMatcherFactory) {
+
+		_defaultScopeMatcherFactory = defaultScopeMatcherFactory;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
@@ -101,6 +155,8 @@ public class AssignScopesMVCRenderCommand implements MVCRenderCommand {
 
 	@Reference
 	private ApplicationDescriptorLocator _applicationDescriptorLocator;
+
+	private ScopeMatcherFactory _defaultScopeMatcherFactory;
 
 	@Reference
 	private DLURLHelper _dlURLHelper;
@@ -122,5 +178,8 @@ public class AssignScopesMVCRenderCommand implements MVCRenderCommand {
 
 	@Reference
 	private ScopeLocator _scopeLocator;
+
+	private ServiceTrackerMap<String, ScopeMatcherFactory>
+		_scopeMatcherFactoriesServiceTrackerMap;
 
 }
